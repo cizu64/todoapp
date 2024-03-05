316909a8545c3526dd87605d3bc51b616a9bfb72
cizu64 <cizu64@gmail.com>
2024-03-05 23:25:12 +0100

    fix pagination

--- app/page.tsx
+++ app/page.tsx
@@ -7,40 +7,41 @@ import { GetTodos, DeleteTodo } from "./action";
 import RemoveTodo from "@/components/deletetodo";
 import CompleteTodo from "@/components/completetodo";
 import Link from "next/link";
+import { Suspense } from "react";
 
 
 //searchParams is a spacial prop that can be use to ssearch querying string in a route
-export default async function Todo({ searchParams }: { searchParams?: { query?: any, pageNum?:number} }) {
-   const todos = await GetTodos(searchParams?.query, searchParams?.pageNum);
-   
-    return (
+export default async function Todo({ searchParams }: { searchParams?: { query?: any, pageNum?: number } }) {
+  const todos = await GetTodos(searchParams?.query, searchParams?.pageNum);
+
+  return (
     <div className="my-20 max-w-md mx-auto">
       <div className="flex">
         <h2 className="text-white text-center p-5 text-2xl w-4/5">My Todos</h2>
         <Link className="text-white" href={"/createtodo"}>
-          <PencilSquareIcon className="hover:text-violet-500 h-10" title="Add Todo"/>
-          </Link>
+          <PencilSquareIcon className="hover:text-violet-500 h-10" title="Add Todo" />
+        </Link>
       </div>
       <SearchTodo />
       <ul role="list" className="divide-y divide-slate-200 bg-white ">
-        {todos.detail.map((todo:any) =>
-          <li key={todo.id} className="flex py-4 first:pt-5 last:pb-5 hover:bg-slate-400">
-            <div className="ml-3 w-2/4 overflow-hidden">
-              <p className="text-sm font-medium text-slate-900">{todo.title}</p>
-              <p className="text-sm text-slate-700 truncate">{todo.description}</p>
-            </div>
-            <div className="flex-column ml-3 overflow-hidden">
-            <small className="text-xs">Done?</small>
-            <CompleteTodo id={todo.id} isComplete={todo.isComplete}/>
-            </div>
-            <div className="ml-3 overflow-hidden">
-             <small className="text-slate-900">{todo.dateCreated}</small>
-            </div>
-            <RemoveTodo id={todo.id}/>
-          </li>
+        {todos.detail.map((todo: any) =>
+            <li key={todo.id} className="flex py-4 first:pt-5 last:pb-5 hover:bg-slate-400">
+              <div className="ml-3 w-2/4 overflow-hidden">
+                <p className="text-sm font-medium text-slate-900">{todo.title}</p>
+                <p className="text-sm text-slate-700 truncate">{todo.description}</p>
+              </div>
+              <div className="flex-column ml-3 overflow-hidden">
+                <small className="text-xs">Done?</small>
+                <CompleteTodo id={todo.id} isComplete={todo.isComplete} />
+              </div>
+              <div className="ml-3 overflow-hidden">
+                <small className="text-slate-900">{todo.dateCreated}</small>
+              </div>
+              <RemoveTodo id={todo.id} />
+            </li>
         )}
       </ul>
-      <Paginate totalPages={todos.meta.totalPages}/>
+      <Paginate totalPages={todos.meta.totalPages} />
     </div>
   );
 }
--- components/pagination.tsx
+++ components/pagination.tsx
@@ -6,29 +6,26 @@ import { useState } from "react";
 export default function Paginate({ totalPages }: { totalPages: any }) {
 
     const pathName = usePathname();
-    const[pageNum,setPageNum] = useState(1);
-    const{replace} = useRouter();
+    const { replace } = useRouter();
 
-
+    let pageNum = 0;
     function Next() {
-        setPageNum(totalPages == pageNum ? pageNum : pageNum + 1)
-        //console.log(pageNum);
+        pageNum = totalPages == pageNum ? pageNum : pageNum + 1;
         replace(`${pathName}?pageNum=${pageNum.toString()}`)
     }
     function Prev() {
-        setPageNum(pageNum == 0? pageNum : pageNum - 1);
-        //console.log(pageNum)
+        pageNum = pageNum == 0 ? pageNum : pageNum - 1;
         replace(`${pathName}?pageNum=${pageNum.toString()}`)
     }
 
     return (
         <div className="max-w-sm mx-auto my-5 rounded-full bg-sky px-2 py-0.5 dark:text-sky-300 dark:border-sky-500/15 dark:bg-sky-500/10 text-center">
             <button onClick={Prev}
-             className="rounded-sm mr-4 p-2 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 text-white w-1/4">Prev</button>
+                className="rounded-sm mr-4 p-2 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 text-white w-1/4">Prev</button>
 
-              <button onClick={Next}
-               className="rounded-sm mr-4 p-2 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 text-white w-1/4">Next</button>
-          </div>
+            <button onClick={Next}
+                className="rounded-sm mr-4 p-2 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 text-white w-1/4">Next</button>
+        </div>
     )
 }
 
